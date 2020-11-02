6298a68c4d3d54490b78c2f479c088ddd52d09ec
Cyril (David) Oram <coram007@gmail.com>
2020-11-02 08:02:28 -0800

    Added Unit Tests to the current code base

--- test/com/games/pieces/test/PlayerTest.java
+++ test/com/games/pieces/test/PlayerTest.java
@@ -1,16 +1,33 @@
 package com.games.pieces.test;
 
 import com.games.pieces.Player;
+
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-//public class PlayerTest {
-//    Player player = new Player();
-//
-//@Test
-//    public void testPlayerHasWeapon(){
-//    player.setInventory("weapon");
-//    String weapon = player.getInventory().get(0);
-//    assertEquals(weapon, "weapon");
-//}
-//}
+
+public class PlayerTest {
+    Player player;
+
+    @Before
+    public void init() {
+        player = new Player('x', null, 0,0);
+    }
+
+    //Positive Test
+    @Test
+    public void testPlayerHasWeapon(){
+        player.setInventory("weapon");
+        String weapon = player.getInventory().get(0);
+        assertEquals(weapon, "weapon");
+    }
+
+    //Negative test
+//    @Test
+//    public void testPlayerHas_InvalidWeapon(){
+//        player.setInventory("weapon");
+//        String weapon = player.getInventory().get(0);
+//        assertNotEquals(weapon, "weapon");
+//    }
+}
